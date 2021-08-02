36259f4b0f24664b85132de54a22a183963fffbe
pedenys <pierreemmanuel.denys@gmail.com>
2021-08-02 09:28:11 +0200

    add link to home page in title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 interface Props {
   title: string
 }
 
 const Title: React.FC<Props> = ({ title }) => {
-  return <h1>{title}</h1>
+  return (
+    <h1>
+      <Link href='/'>{title}</Link>
+    </h1>
+  )
 }
 
 export default Title
