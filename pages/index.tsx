966f1dd6d607c536ee28867f92a37e7bcbe05e3e
pedenys <pierreemmanuel.denys@gmail.com>
2021-08-02 09:27:58 +0200

    use routing instead of state for navigation between chapters

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,35 @@
 import Head from 'next/head'
-import React, { FormEvent, useEffect, useRef, useState } from 'react'
+import React, { FormEvent, useRef, useState } from 'react'
 import content from '../public/manuel.json'
 import Chapter from '../src/components/Chapter'
 import Menu from '../src/components/Menu'
 import Title from '../src/components/Title'
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
 
 const chapters = content.chapters
 const firstChapter = chapters[0]
 const maxLengthChapters = chapters.length
 
 export default function Home() {
-  const [currentChapterId, setCurrentChapterId] = useState<string>(
-    firstChapter.id
-  )
-
+  const router = useRouter()
+  const { chapter } = router.query
+  const chapterToDisplay =
+    chapters.find((chap) => chap.id === chapter) ?? firstChapter
   const chapterNumberInputRef = useRef<HTMLInputElement>(null)
 
   /**
    * Navigate to next chapter if it exists
    */
   const handleNextChapter = (): void => {
-    const potentialNextChapter: string = (
-      Number(currentChapterId) + 1
-    ).toString()
+    const potentialNextChapter: string = (Number(chapter) + 1).toString()
     if (chapters.find((chap) => chap.id === potentialNextChapter)) {
-      setCurrentChapterId(potentialNextChapter)
+      router.push({
+        pathname: '',
+        query: {
+          chapter: potentialNextChapter,
+        },
+      })
     }
   }
 
@@ -33,28 +37,17 @@ export default function Home() {
    * Navigate to previous chapter if it exists
    */
   const handlePreviousChapter = (): void => {
-    const potentialPreviousChapter: string = (
-      Number(currentChapterId) - 1
-    ).toString()
+    const potentialPreviousChapter: string = (Number(chapter) - 1).toString()
     if (chapters.find((chap) => chap.id === potentialPreviousChapter)) {
-      setCurrentChapterId(potentialPreviousChapter)
+      router.push({
+        pathname: '',
+        query: {
+          chapter: potentialPreviousChapter,
+        },
+      })
     }
   }
 
-  /**
-   * Display only chapter one by one
-   */
-  const buildChapterByChapterContent = (): JSX.Element => {
-    const chapterToDisplay =
-      chapters.find((chap) => chap.id === currentChapterId) ?? firstChapter
-    return (
-      <Chapter
-        id={chapterToDisplay?.id}
-        paragraphs={chapterToDisplay?.content}
-        title={chapterToDisplay.title}
-      />
-    )
-  }
   /**
    * Floor chapter number provided by user and if legal (a number && < max chapter number)
    */
@@ -70,7 +63,12 @@ export default function Home() {
       typeof roundedChapterNumber === 'number' &&
       roundedChapterNumber <= maxLengthChapters
     ) {
-      setCurrentChapterId(roundedChapterNumber.toString())
+      router.push({
+        pathname: '',
+        query: {
+          chapter: roundedChapterNumber.toString(),
+        },
+      })
     }
   }
 
@@ -93,7 +91,13 @@ export default function Home() {
           />
         </div>
       </header>
-      <main className={styles.main}>{buildChapterByChapterContent()}</main>
+      <main className={styles.main}>
+        <Chapter
+          id={chapterToDisplay?.id}
+          paragraphs={chapterToDisplay?.content}
+          title={chapterToDisplay.title}
+        />
+      </main>
     </div>
   )
 }
